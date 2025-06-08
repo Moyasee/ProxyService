da8da554bc531905d1b01565c9dbcb6eedd73331
zxcsix-zxc <terranoiz@icloud.com>
2025-06-08 21:29:43 +0300

    123

--- proxyservice/api/health.js
+++ proxyservice/api/health.js
@@ -1,4 +1,5 @@
-module.exports = async function handler(req, res) {  // Set CORS headers
+module.exports = async function handler(req, res) {
+  // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
